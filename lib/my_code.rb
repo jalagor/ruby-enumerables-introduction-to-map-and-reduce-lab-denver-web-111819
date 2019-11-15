471f463ecdab43aaff0bd613c7d28d7e848fe9af
John Rogala <johnrossrogala@gmail.com>
2019-11-15 11:13:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ def reduce_to_total(source_array)
   total = 0 
   counter = 0 
   while counter < source_array.count do 
-    total + source_array[counter]
+    total =+ source_array[counter]
   end
   puts total 
 end
