2ac0347055c56a629dca2866cc79a89b611d9466
John Rogala <johnrossrogala@gmail.com>
2019-11-15 11:54:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,8 +68,7 @@ def reduce_to_any_true(source_array)
   while counter < source_array.count do 
     if source_array[counter] == true
       return true 
-    else
-      return false
+     
     end
     counter += 1 
   end
