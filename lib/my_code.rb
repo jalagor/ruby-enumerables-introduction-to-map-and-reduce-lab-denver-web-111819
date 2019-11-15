afb73ce3f7dd21a5c20a1c308f38ca7a3fdf7f3b
John Rogala <johnrossrogala@gmail.com>
2019-11-15 11:18:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,9 +39,13 @@ def map_to_square(source_array)
   new_array
 end 
  
-def reduce_to_total(source_array)
+def reduce_to_total(source_array, starting_point)
   total = 0 
-  counter = 0 
+  if starting_point 
+    starting_point = counter
+  else
+    counter = 0  
+  end
   while counter < source_array.count do 
     total += source_array[counter] 
     counter += 1
